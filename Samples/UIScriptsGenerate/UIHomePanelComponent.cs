cfb98bfe1aaba1c45f6e8e80c4fce4c23da861bc
ZZQ <zhiqiangz@opera.com>
2024-12-20 16:14:48 +0800

    添加包模块

--- Samples/UIScriptsGenerate/UIHomePanelComponent.cs
+++ Samples/UIScriptsGenerate/UIHomePanelComponent.cs
@@ -7,20 +7,23 @@ namespace GameUI
 	{
 		#region Auto Generate Code
 		private UnityEngine.UI.Button StartButton;
+		private UnityEngine.UI.Image AboutImage;
 		private UnityEngine.UI.Button AboutButton;
 		private UnityEngine.UI.Text VersionText;
 		private UnityEngine.RectTransform AboutViewRectTransform;
+		private UnityEngine.UI.Image MaskImage;
 		private UnityEngine.UI.Button MaskButton;
 
 		public void InitData()
 		{
 			StartButton = transform.Find("@Start").GetComponent<UnityEngine.UI.Button>();
+			AboutImage = transform.Find("@About").GetComponent<UnityEngine.UI.Image>();
 			AboutButton = transform.Find("@About").GetComponent<UnityEngine.UI.Button>();
 			VersionText = transform.Find("@version").GetComponent<UnityEngine.UI.Text>();
 			AboutViewRectTransform = transform.Find("@AboutView").GetComponent<UnityEngine.RectTransform>();
+			MaskImage = AboutViewRectTransform.transform.Find("@mask").GetComponent<UnityEngine.UI.Image>();
 			MaskButton = AboutViewRectTransform.transform.Find("@mask").GetComponent<UnityEngine.UI.Button>();
 		}
-
 		#endregion Auto Generate Code
 	}
 }
