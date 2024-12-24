32f314d66f4a78cd709ef6ae7f3c4750b3f8ac6e
ZZQ <zhiqiangz@opera.com>
2024-12-24 19:28:07 +0800

    添加对象池

--- RunTime/GameObjectPool/GameObjectPool.cs
+++ RunTime/GameObjectPool/GameObjectPool.cs
@@ -3,38 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class GameObjectPool
+namespace GameUI
 {
-    private static GameObjectPool _instance;
-    
-    public static GameObjectPool Instance
+    public enum PoolType
     {
-        get
+        Stack,
+        LinkedList
+    }
+    public class GameObjectPool
+    {
+        private static GameObjectPool _instance;
+        public static GameObjectPool Instance
         {
-            if (_instance == null)
+            get
             {
-                _instance = new GameObjectPool();
+                if (_instance == null)
+                {
+                    _instance = new GameObjectPool();
+                }
+                return _instance;
             }
-            return _instance;
         }
+        
+        public PoolType PoolType;
+
+        // Collection checks will throw errors if we try to release an item that is already in the pool.
+        public static bool CollectionChecks = true;
+        public static int MaxPoolSize = 10;
+        
+        private IObjectPool<GameObject> _pool;
+
+        public IObjectPool<GameObject> Pool
+        {
+            get
+            {
+                if (_pool == null)
+                {
+                    if (PoolType == PoolType.Stack)
+                    {
+                        _pool = new ObjectPool<GameObject>(CreatePooledItem,OnTakeFromPool,OnReturnedToPool,OnDestroyPoolObject,CollectionChecks,10,MaxPoolSize);
+                    }
+                    else
+                    {
+                        _pool = new LinkedPool<GameObject>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, CollectionChecks, MaxPoolSize);
+                    }
+                }
+
+                return _pool;
+            }
+        }
+        
+        
+        private static GameObject CreatePooledItem()
+        {
+            return null;
+        }
+    
+        private static void OnTakeFromPool(GameObject obj)
+        {
+            throw new System.NotImplementedException();
+        }
+        private static void OnReturnedToPool(GameObject obj)
+        {
+            throw new System.NotImplementedException();
+        }
+        private static void OnDestroyPoolObject(GameObject obj)
+        {
+            throw new System.NotImplementedException();
+        }
+        
     }
 
-    public ObjectPool<GameObject> pool = new ObjectPool<GameObject>(CreateFunc,OnGet,OnRelease,OnObjDestroy);
-
-    private static void OnObjDestroy(GameObject obj)
-    {
-        throw new System.NotImplementedException();
-    }
-    private static void OnRelease(GameObject obj)
-    {
-        throw new System.NotImplementedException();
-    }
-    private static void OnGet(GameObject obj)
-    {
-        throw new System.NotImplementedException();
-    }
-    private static GameObject CreateFunc()
-    {
-        throw new System.NotImplementedException();
-    }
 }
